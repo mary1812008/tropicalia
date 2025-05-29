6ac11b2cee9d5c9fd7623f81a21b37bec236bfd8
mary1812008 <164516580+mary1812008@users.noreply.github.com>
2025-05-29 10:48:22 -0300

    1812

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById ('opcao-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener("click")
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList. toggle('apresenta-lista')
+
+    })
