d0898d1fc82582ef35c1dab3dd04d00331bb09b7
mary1812008 <164516580+mary1812008@users.noreply.github.com>
2025-06-05 09:12:15 -0300

    a

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById ('opcao-acessibilidade')
 
-    botaoDeAcessibilidade.addEventListener("click")
+    botaoDeAcessibilidade.addEventListener('click' function() {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList. toggle('apresenta-lista')
 
