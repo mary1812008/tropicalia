c84bbbff236871c20e233395917d4e27ce686cd3
mary1812008 <164516580+mary1812008@users.noreply.github.com>
2025-06-05 09:43:21 -0300

    b

--- script.js
+++ script.js
@@ -7,5 +7,18 @@ document.addEventListener('DOMContentLoaded', function(){
         opcoesDeAcessibilidade.classList. toggle('apresenta-lista')
 
     })
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    const alternaContraste = document.getElementById('alterna-contraste')
     
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFone += 0.1;
+        document.body.style.fontSize = `$ {tamanhoatualFonte} rem`
+    })
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle("alto-contraste")
+    })
+})
